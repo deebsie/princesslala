b419816b24ad72ecb40958fa468a5d36e5621a88
deebsie <135828695+deebsie@users.noreply.github.com>
2025-10-05 12:35:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ const messages = [
     "Are you sure?",
     "Really sure??",
     "Are you positive?",
-    "Lala please...",
+    "Lala please TT...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
